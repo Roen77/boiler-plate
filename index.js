53538f04792804098f5372ffa82575a433c6457b
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-03-09 18:21:56 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+
+const config=require("./config/key")
+
+const {User}=require('./models/User')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 
 const mongoose=require('mongoose')
 mongoose.connect(
-    'mongodb+srv://roenhan:exma@clusterroen.an4ms.mongodb.net/roenhan?retryWrites=true&w=majority',
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology:true,
@@ -15,7 +24,22 @@ mongoose.connect(
 .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!!')
+})
+
+app.post('/register',(req,res)=>{
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터베이스에 넣어준다
+    //req.body는 객체형식으롤 들어온다
+    const user=new User(req.body);
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
 })
 
 app.listen(port, () => {
